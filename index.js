ddd32ac9ef7ff219e6ba4a32f9bb70aa04a37ce0
gbrllim <gbrllim@gmail.com>
2023-10-26 16:51:01 +0800

    basic backend with individual sighting data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const { getSightings } = require("./utils.js");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(cors());
 
 app.get("/", async (req, res) => {
   res.send("hello Rocket");
@@ -11,7 +14,26 @@ app.get("/", async (req, res) => {
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
-  res.json(sightings);
+  const sightingsList = sightings.map((sighting) => sighting.REPORT_NUMBER);
+
+  sightingsList.sort((a, b) => {
+    return parseInt(a) - parseInt(b);
+  });
+
+  res.json(sightingsList);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightingIndex = req.params.sightingIndex;
+  const sightings = await getSightings();
+
+  // Ensure sightingIndex within range
+  if (sightingIndex >= 0 && sightingIndex < sightings.length) {
+    const requestedSighting = sightings[sightingIndex];
+    res.json(requestedSighting);
+  } else {
+    res.status(404).json({ error: "Sighting not found" });
+  }
 });
 
 app.listen(PORT, () => {
